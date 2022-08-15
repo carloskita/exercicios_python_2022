87e637b9017ce54de9517fdadeda89a92015c721
Carlos Kitazawa Neto <carloskitazawa@hotmail.com>
2022-08-15 19:08:05 -0300

    ate a 11

---
+++ aula_1/exercicio_11.py
@@ -0,0 +1,7 @@
+readA = float(input('Digite um numero: '))
+readB = float(input('Digite outro numero: '))
+readC = int(input('Digite um numero inteiro: '))
+
+print('O produto do dobro do primeiro com metade do segundo: %.1f' % ((readA * readA) * (readB / 2)))
+print('A soma do triplo do primeiro com o terceiro: %.1f' % ((readA * 3) + readC))
+print('O terceiro elevado ao cubo: %.1f' % (readC ** 4))
---
+++ aula_1/exercicio_9.py
@@ -0,0 +1,4 @@
+readFaren = float(input('Digite a temperatura em fahrenheint para a conversao: '))
+convertCelsius = 5 * ((readFaren-32) / 9)
+
+print('Convertido para celsius, a temperatura fica: %.1f', convertCelsius)
